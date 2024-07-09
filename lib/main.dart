12caa64bcb6d7f2edf3cfcd569d6b64fe438af59
Szymon Łukiewicz <33434971+Szirle@users.noreply.github.com>
2024-07-10 00:16:15 +0200

    UI update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:psych_gen_app/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -19,8 +21,43 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TestApiWidget(),
+      // home: const MyHomePage(title: 'Flutt`er Demo Home Page'),
     );
+  }
+}
+
+class TestApiWidget extends StatefulWidget {
+  @override
+  _TestApiWidgetState createState() => _TestApiWidgetState();
+}
+
+class _TestApiWidgetState extends State<TestApiWidget> {
+  String _message = 'Fetching data...';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    final response = await http.get(Uri.parse('/test'));  // Using a relative URL
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _message = json.decode(response.body)['message'];
+      });
+    } else {
+      setState(() {
+        _message = 'Failed to load data!';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(_message);
   }
 }
 
