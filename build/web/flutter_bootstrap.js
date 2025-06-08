90a4f364a8c86d276fc03c3cda25660a8ccfbb8e
Szymon Łukiewicz <33434971+Szirle@users.noreply.github.com>
2025-06-08 23:44:37 +0200

    new build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1966853114"
+    serviceWorkerVersion: "1330936383"
   }
 });
