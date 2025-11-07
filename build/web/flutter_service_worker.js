99d2f0416d99acea0caa6bd57ef3a29de8dd4b2c
Szymon Łukiewicz <33434971+Szirle@users.noreply.github.com>
2025-11-07 12:54:25 +0100

    Add nccl

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f13cb3eaaa8c6d9057744308572559d3",
+const RESOURCES = {"flutter_bootstrap.js": "e1a3d0c3cf7852d2194ee36b7d2237ef",
 "version.json": "80ac3aea40b0d68268e2f9f043068a01",
-"index.html": "38dc00569dc65fb942be9339adedf16c",
-"/": "38dc00569dc65fb942be9339adedf16c",
+"index.html": "856af6824b5f56f1f52e213e7fe5543a",
+"/": "856af6824b5f56f1f52e213e7fe5543a",
 "main.dart.js": "d508e041fc762588c4be894fa264dd10",
 "404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
